8196d97a90082e0fd833791204ccf3b1fd22285d
NguyenCuong <23520200@gm.uit.edu.vn>
2025-05-09 20:34:12 +0700

    sec

--- SJF.cpp
+++ SJF.cpp
@@ -38,11 +38,45 @@ int main()
 
     while (completed < n)
     {
-        
+        int idx = -1;
+        int minBurst = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            if (!processes[i].done && processes[i].iArrival <= currentTime)
+            {
+                if (processes[i].iBrust < minBurst)
+                {
+                    minBurst = processes[i].iBrust;
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1)
+        {
+            currentTime++;
+            continue;
+        }
+
+        processes[idx].iStart = currentTime;
+        processes[idx].iFinish = currentTime + processes[idx].iBrust;
+        processes[idx].iTaT = processes[idx].iFinish - processes[idx].iArrival;
+        processes[idx].iWaiting = processes[idx].iTaT - processes[idx].iBrust;
+        processes[idx].iRespone = processes[idx].iStart - processes[idx].iArrival;
+
+        currentTime += processes[idx].iBrust;
+        processes[idx].done = true;
+        result.push_back(processes[idx]);
+
+        totalTaT += processes[idx].iTaT;
+        totalWaiting += processes[idx].iWaiting;
+        totalResponse += processes[idx].iRespone;
+        completed++;
     }
     cout << "PID\tArrival\tBurst\tStart\tFinish\tTaT\tWaiting\tResponse\n";
-    for (auto &p : result)
+    for (int i = 0; i < n; i++)
     {
+        PCB p = result[i];
         cout << p.iPID << '\t' << p.iArrival << '\t' << p.iBrust << '\t' << p.iStart << '\t' << p.iFinish << '\t' << p.iTaT << '\t' << p.iWaiting << '\t' << p.iRespone << '\n';
     }
 
